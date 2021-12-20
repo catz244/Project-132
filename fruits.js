55a341f4c78cd8fb756b7d1be5fedc616cee6955
catz244 <78543753+catz244@users.noreply.github.com>
2021-12-20 13:38:36 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,32 @@
+img = "";
+Status = "";
+
+function setup() {
+    canvas = createCanvas(430, 330);
+    canvas.position(420, 165);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function preload() {
+    img = loadImage("fruits.jpg");
+}
+
+function draw() {
+    image(img, 0, 0, 430, 330);
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
+}
